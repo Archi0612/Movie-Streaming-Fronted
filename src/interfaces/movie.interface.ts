3d67b04720cf96457743f1296b962787d3db81ff
Priyanshu <priyanshu@Priyanshus-MacBook-Pro.local>
2025-02-18 12:00:20 +0530

    ..

--- src/interfaces/movie.interface.ts
+++ src/interfaces/movie.interface.ts
@@ -1,58 +1,66 @@
 // src/interfaces/movie.interface.ts
 
 export interface Movie {
-    id: number;
-    title: string;
-    poster_path: string;
-    backdrop_path: string;
-    overview: string;
-    release_date: string;
-    vote_average: number;
-    genre_ids: number[];
-    original_language: string;
-    popularity: number;
-    vote_count: number;
-    adult: boolean;
-    video: boolean;
-    
-  }
+  id: number;
+  title: string;
+  poster_path: string;
+  backdrop_path: string;
+  overview: string;
+  release_date: string;
+  vote_average: number;
+  genre_ids: number[];
+  original_language: string;
+  popularity: number;
+  vote_count: number;
+  adult: boolean;
+  video: boolean;
 
- export interface TrendingMoviesProps {
-    movies: Movie[];
-  }
-  
-  export interface MovieCardProps {
-    title: string;
-    posterPath: string;
-    overview: string;
-    releaseDate: string;
-    voteAverage: number;
-    language: string;
-    genres_id: number[];
-  }
-  
-  export interface MovieGridProps {
-    movies: Movie[];
-    title: string;
-  }
-  
-  export interface MovieResponse {
-    page: number;
-    results: Movie[];
-    total_pages: number;
-    total_results: number;
-  }
-  
-  export interface VideoResult {
-    id: string;
-    key: string;
-    name: string;
-    site: string;
-    size: number;
-    type: string;
-  }
-  
-  export interface VideoResponse {
-    id: number;
-    results: VideoResult[];
-  }
+}
+
+export interface TrendingMoviesProps {
+  movies: Movie[];
+}
+
+export interface MovieCardProps {
+  title: string;
+  posterPath: string;
+  overview: string;
+  releaseDate: string;
+  voteAverage: number;
+  language: string;
+  genres_id: number[];
+}
+
+export interface MovieGridProps {
+  movies: Movie[];
+  title: string;
+}
+
+export interface MovieResponse {
+  page: number;
+  results: Movie[];
+  total_pages: number;
+  total_results: number;
+}
+
+export interface VideoResult {
+  id: string;
+  key: string;
+  name: string;
+  site: string;
+  size: number;
+  type: string;
+}
+
+export interface VideoResponse {
+  id: number;
+  results: VideoResult[];
+}
+
+export interface Profile {
+  name: string;
+  gender: string;
+  dob: string;
+  country: string;
+  email: string;
+}
