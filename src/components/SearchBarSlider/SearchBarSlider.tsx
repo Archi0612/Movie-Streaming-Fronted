730912cf625e1bbcc952bfa0bc96f5a34a4fce6d
Priyanshu <priyanshu@Priyanshus-MacBook-Pro.local>
2025-03-17 18:22:58 +0530

    Before merge

--- src/components/SearchBarSlider/SearchBarSlider.tsx
+++ src/components/SearchBarSlider/SearchBarSlider.tsx
@@ -6,25 +6,15 @@ import "./SearchBarSlider.css";
 const MovieCardSlider: React.FC<MediaGridProps> = ({ mediaList, title }) => {
     const sliderRef = useRef<HTMLDivElement>(null);
 
-    // const scrollLeft = () => {
-    //     if (sliderRef.current) {
-    //         sliderRef.current.scrollBy({ left: -300, behavior: "smooth" });
-    //     }
-    // };
-
-    // const scrollRight = () => {
-    //     if (sliderRef.current) {
-    //         sliderRef.current.scrollBy({ left: 300, behavior: "smooth" });
-    //     }
-    // };
+    console.log(mediaList, "mediaList");
+    console.log(title, "title"); 
 
     return (
         <div className="searchbarslidermain_container">
             <h2 className="searchbarslidermain_title">{title}</h2>
             <div className="slider-container">
-
                 <div className="searchmovie-slider" ref={sliderRef}>
-                    {mediaList.map((media) => (
+                    {mediaList?.map((media) => (
                         <div key={media._id} className="movie-slide">
                             <MovieCard media={media} />
                         </div>
@@ -33,8 +23,6 @@ const MovieCardSlider: React.FC<MediaGridProps> = ({ mediaList, title }) => {
 
             </div>
         </div>
-
-
     );
 };
 
