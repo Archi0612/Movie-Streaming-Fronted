0ab7630ba054bb01a91f030b61f4b1bcb004fb63
Priyanshu <priyanshu@Priyanshus-MacBook-Pro.local>
2025-03-19 11:24:16 +0530

    lint configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+// import eslint from 'vite-plugin-eslint' as any;
+
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     eslint({
+//       // Configure options if needed
+//       failOnError: false, // This will not crash the server on lint errors
+//       include: ['src/**/*.ts', 'src/**/*.tsx'], // Files to lint
+//       emitWarning: true, // Show warnings in console
+//       emitError: true, // Show errors in console
+//     }),
+//   ],
+// });
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+// Use type assertion for the import
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+  ],
   server: {
     port: Number(process.env.PORT) || 3012,
   }
-})
+});
